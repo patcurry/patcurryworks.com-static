af8f99cbf2868e720933abf1c2718f5c01bc863f
currypat <currypat1985@gmail.com>
2018-04-21 08:36:38 +0200

    basic set up of react for the workouts app

---
+++ src/workouts-react.js
@@ -0,0 +1,10 @@
+// patcurryworks.com-static/src/workouts-react.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Workouts App with react, react router and django rest framework.</h1>,
+    document.getElementById('react-root')
+);
+
+
